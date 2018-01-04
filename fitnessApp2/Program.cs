6409b3faa00850d7dd8a8b29c8d5a0785429f80b
Samuel Lindsey <samuel.lindsey@icloud.com>
2018-01-03 20:25:53 -0500

    Initial check-in of module fitnessApp2

---
+++ fitnessApp2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace fitnessApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double runningTotal = 0;
+
+            while (true)
+            {
+                // Prompt user for minutes exercised 
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+                string entry = Console.ReadLine();
+
+                if (entry.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    // Add minutes exercised to total 
+                    double minutes = double.Parse(entry);
+
+                    if (minutes <= 0)
+                    {
+                        Console.WriteLine(minutes + " is not an acceptable value.");
+                        continue;
+                    }
+                    else if (minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Okay, now you're just showing off!");
+                    }
+
+                    runningTotal += minutes;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not valid input.");
+                    continue;
+                }
+
+                // Display total minutes exercised to the screen 
+                Console.WriteLine("You've entered " + runningTotal + " minutes.");
+
+                // Repeat until user quits
+            }
+
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
